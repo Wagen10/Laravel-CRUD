f1c896c2c6bdd318dc0d9aabbb5a312fe0daf265
Wagen10 <111834404+Wagen10@users.noreply.github.com>
2022-12-02 14:30:02 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\castController;
+use App\Http\Controllers\layout;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [layout ::class,'index']);
+
+Route::controller(layout::class)->group(function(){
+    Route::get('/admin/table','home');
+    Route::get('/admin/main','index');
+    Route::get('/admin/data-tables','data');
+});
+
+Route::get('/admin/home', function () {
+    return view('admin/home');
+});
+
+Route::resource('cast',castController::class);
